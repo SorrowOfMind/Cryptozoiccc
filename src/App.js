f68a7204a6d48b7ffd7a32756bc0982f46ac44a9
SorrowOfMind <sortoflifecraze@gmail.com>
2022-02-24 20:55:51 +0100

    setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+
+import {Typography, Space, Layout} from 'antd';
+
+const App = () => {
+  return (
+    <div className="app">App</div>
+  )
+}
+
+export default App
