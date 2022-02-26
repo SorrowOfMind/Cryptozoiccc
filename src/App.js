f97029e910fe66eed4368345ff513ead3de27ea1
SorrowOfMind <sortoflifecraze@gmail.com>
2022-02-26 15:00:06 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import {Switch, Route, Link} from 'react-router-dom';
 
 import {Typography, Space, Layout} from 'antd';
 
+import {Navbar} from './components';
+
+import './App.css';
+
 const App = () => {
   return (
-    <div className="app">App</div>
+    <div className="app">
+      <div className="navbar">
+        <Navbar /> 
+      </div>
+      <main className="main">
+
+      </main>
+      <footer className="footer">
+
+      </footer>
+    </div>
   )
 }
 
