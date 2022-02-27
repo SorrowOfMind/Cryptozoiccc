614f3060e17a9f0782021f15249c101c3f7e31f1
SorrowOfMind <sortoflifecraze@gmail.com>
2022-02-27 12:25:52 +0100

    api

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import millify from 'millify';
+import { Typography, Col, Row, Statistic } from 'antd';
+import {Link} from 'react-router-dom';
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
+const {Title} = Typography;
+
 
 const Home = () => {
+  // const {data, isFetching} = useGetCryptosQuery();
+  const globalStats = data?.data?.stats;
+
+  if (isFetching) return 'Loading...';
+
   return (
-    <div>Home</div>
+    <>
+        <Title level={2} className="heading">Global Crypto Stats</Title>
+        <Row>
+            <Col span={12}>
+                <Statistic title="Total Cryptocurrencies" value={globalStats.total}/>
+                <Statistic title="Total Exchanges" value={globalStats.exchanges} />
+                <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/>
+                <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} />
+                <Statistic title="Total Cryptocurrencies" value={millify(globalStats.totalMarkets)} />
+            </Col>
+        </Row>
+    </>
   )
 }
 
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const apiHeaders = {
+    'content-type': 'application/json',
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': ''
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = url => ({url, headers: apiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery
+} = cryptoApi;
